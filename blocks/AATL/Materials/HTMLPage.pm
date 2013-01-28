2b285b5fc5bc85889da67c1b4bc5fa157e2e72d3
Chris <chris@starforge.co.uk>
2013-01-28 15:16:03 +0000

    Angle brakets in gpl license URL (thanks jerith)

--- blocks/AATL/Materials/HTMLPage.pm
+++ blocks/AATL/Materials/HTMLPage.pm
@@ -14,7 +14,7 @@
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# along with this program.  If not, see http://www.gnu.org/licenses/.
 
 ## @class
 # Allow the user to add a HTML page (popup, really) to the materials page.
--- modules/AATL/BlockSelector.pm
+++ modules/AATL/BlockSelector.pm
@@ -15,7 +15,7 @@
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# along with this program.  If not, see http://www.gnu.org/licenses/.
 
 ## @class
 # Select the appropriate block to render a page based on an AATL URL.
